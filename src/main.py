c13423b9af4e5894a4589802713326772324421f
Lê Vũ Quang <quanglv.341@gmail.com>
2023-07-20 00:58:11 +0700

    wip

--- src/main.py
+++ src/main.py
@@ -1,14 +1,39 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont, QIcon, QImage, QImageReader, QMovie, QPixmap, QRegion
+from PyQt6.QtWidgets import QApplication, QComboBox, QGridLayout, QLabel, QWidget
 
-app = QApplication([])
 
-window = QWidget()
-window.setWindowTitle("PyQt App")
-window.setGeometry(100, 100, 280, 80)
-helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
-helloMsg.move(60, 15)
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
 
+        self.setGeometry(200, 200, 700, 400)
+        self.setWindowTitle("Python Image Compression Tools")
+
+        layout = QGridLayout()
+
+        helloMsg = QLabel("<h1>Hello, World!</h1>", parent=self)
+        originalImage = QLabel()
+        encodedImage = QLabel()
+
+        self.pageCombo = QComboBox()
+        self.pageCombo.addItems(["PNG", "JPEG-LS"])
+        self.pageCombo.activated.connect(self.switchPage)
+
+        layout.addWidget(helloMsg, 0, 0)
+        layout.addWidget(originalImage, 1, 0)
+        layout.addWidget(encodedImage, 1, 1)
+        self.setLayout(layout)
+
+        pixmap = QPixmap("data/big_tree.ppm")
+        pixmap = pixmap.scaled(500, 500, Qt.AspectRatioMode.KeepAspectRatio)
+        originalImage.setPixmap(pixmap)
+        originalImage.show()
+
+
+app = QApplication(sys.argv)
+window = Window()
 window.show()
 sys.exit(app.exec())
